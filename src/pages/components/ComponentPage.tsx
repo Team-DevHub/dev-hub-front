5288dbe89795bb9bf615947d59f2b2bd508403f1
JMK1007 <coxld010@naver.com>
2024-05-10 03:00:39 +0900

    Style: 컴포넌트 테스트 페이지 스타일 수정

--- src/pages/components/ComponentPage.tsx
+++ src/pages/components/ComponentPage.tsx
@@ -3,6 +3,7 @@ import FormButton from '@/components/common/FormInput/FormButton';
 import FormInput from '@/components/common/FormInput/FormInput';
 import { FormRegex } from '@/utils/regex';
 import { useState } from 'react';
+import styled from 'styled-components';
 
 const ComponentPage = () => {
   const [input, setInput] = useState('');
@@ -10,28 +11,38 @@ const ComponentPage = () => {
 
   return (
     <div>
-      <FormInput
-        id='email-input'
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        label={'Email'}
-        regex={FormRegex.email}
-        errorMessage='유효하지 않은 이메일입니다'
-        placeholder='이메일을 입력해주세요'
-      />
-      <Checkbox
-        id={'checkbox'}
-        checked={checked}
-        label='로그인 정보 저장'
-        onClick={() => setChecked(!checked)}
-      />
-      <FormButton
-        text={'로그인'}
-        onClick={function (): void {
-          throw new Error('Function not implemented.');
-        }}
-      />
+      <Container>
+        <FormInput
+          id='email-input'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          label={'Email'}
+          regex={FormRegex.email}
+          errorMessage='유효하지 않은 이메일입니다'
+          placeholder='이메일을 입력해주세요'
+        />
+      </Container>
+      <Container>
+        <Checkbox
+          id={'checkbox'}
+          checked={checked}
+          label='로그인 정보 저장'
+          onClick={() => setChecked(!checked)}
+        />
+      </Container>
+      <Container>
+        <FormButton
+          text={'로그인'}
+          onClick={function (): void {
+            throw new Error('Function not implemented.');
+          }}
+        />
+      </Container>
     </div>
   );
 };
 export default ComponentPage;
+
+const Container = styled.div`
+  padding: 20px;
+`;
