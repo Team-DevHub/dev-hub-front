c127c40db9208cadd60bf7ddcec1bf76095f579e
JIMIN1020 <jimins4920@gmail.com>
2024-05-11 14:37:49 +0900

    [Style/#5]: 지식 공유하기 버튼

--- src/components/homePage/SideBar.tsx
+++ src/components/homePage/SideBar.tsx
@@ -1,9 +1,17 @@
 import styled from "styled-components";
 import Profile from "./Profile";
+import Button from "../common/Button";
+import PencilIcon from "@/assets/icon/pencil-icon.svg";
 
 function SideBar() {
   return (
     <Container>
+      <Button
+        size="medium"
+        text="지식 공유하기"
+        onClick={() => {}}
+        icon={PencilIcon}
+      />
       <Profile />
     </Container>
   );
@@ -14,4 +22,11 @@ export default SideBar;
 const Container = styled.aside`
   width: 220px;
   display: flex;
+  flex-direction: column;
+  gap: 36px;
+  padding: 24px 0;
+
+  position: sticky;
+  top: 0;
+  left: 0;
 `;
