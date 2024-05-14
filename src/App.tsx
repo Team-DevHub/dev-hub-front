057795689e87b6fc3f54bc46bbb78818d11f542a
연하영 <dusgkdud7@naver.com>
2024-05-14 13:50:07 +0900

    [Refactoring/#4] 마이페이지 경로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import { ThemeProvider } from "styled-components";
-import { theme } from "./styles/theme";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import { GlobalStyle } from "./styles/GlobalStyle";
-import Header from "./components/common/Header";
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import { GlobalStyle } from './styles/GlobalStyle';
+import Header from './components/common/Header';
 import ComponentPage from './pages/components/ComponentPage';
-import MyPage from "./pages/MyPage";
+import MyPage from './pages/MyPage';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <HomePage />,
   },
   {
-    path :"/mypage",
-    element: <MyPage/>
+    path: '/my-page',
+    element: <MyPage />,
   },
   {
-    path: "test",
+    path: 'test',
     children: [
       { index: true, element: <div>test</div> },
-      { path: "nesting", element: <div>/test/nesting</div> },
+      { path: 'nesting', element: <div>/test/nesting</div> },
     ],
   },
   {
