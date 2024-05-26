659d021f89d2075d279362e4300eb49e5d73975f
연하영 <dusgkdud7@naver.com>
2024-05-27 02:52:06 +0900

    [feat/#19] TOAST UI editor 적용

--- src/components/posting/ContentInput.tsx
+++ src/components/posting/ContentInput.tsx
@@ -1,17 +1,43 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import styled from 'styled-components';
+import { Editor } from '@toast-ui/react-editor';
+import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
+import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
+import prism from 'prismjs';
+import 'prismjs/themes/prism.css';
+import '@toast-ui/editor/toastui-editor.css';
+import '@toast-ui/editor/dist/i18n/ko-kr';
+import { useRef } from 'react';
+
+const toolbar = [
+  ['bold', 'italic', 'strike'],
+  ['hr', 'quote', 'ul', 'ol'],
+  ['link'],
+  ['codeblock'],
+];
 
 function ContentInput() {
-  const [value, setValue] = useState<string>('');
+  const editorRef = useRef<Editor>(null);
+
+  const handleOnChange = () => {
+    const data = editorRef.current?.getInstance().getHTML() || '';
+    console.log(data);
+  };
 
   return (
     <Container>
       <h2>공유할 내용</h2>
-      <StyledInput
-        type='text'
-        value={value}
-        placeholder='내용를 입력하세요.'
-        onChange={(e) => setValue(e.target.value)}
+
+      <Editor
+        toolbarItems={toolbar}
+        initialEditType='wysiwyg'
+        initialValue='내용을 입력해 주세요.'
+        hideModeSwitch
+        height='400px'
+        language='ko-KR'
+        ref={editorRef}
+        onChange={handleOnChange}
+        plugins={[[codeSyntaxHighlight, { highlighter: prism }]]}
       />
     </Container>
   );
@@ -24,15 +50,4 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   gap: 15px;
-`;
-
-const StyledInput = styled.input`
-  width: 100%;
-  padding: 15px;
-
-  color: ${({ theme }) => theme.color_textBlack};
-  font-size: ${({ theme }) => theme.fontSize_md};
-  background-color: ${({ theme }) => theme.color_bgWhite};
-  border: 1px solid ${({ theme }) => theme.color_borderGray};
-  border-radius: 8px;
 `;
