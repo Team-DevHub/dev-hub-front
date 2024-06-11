aad296503fefed737677cf6aa8b2eb5d6c4bca6d
연하영 <dusgkdud7@naver.com>
2024-06-11 12:56:24 +0900

    [feat/#37] Comment Empty 문구 추가

--- src/components/modal/CommentList.tsx
+++ src/components/modal/CommentList.tsx
@@ -7,6 +7,9 @@ function CommentList() {
 
   return (
     <Container>
+      {selectedPost?.totalComments === 0 && (
+        <CommentEmpty>아직 댓글이 없어요</CommentEmpty>
+      )}
       {selectedPost?.comments.map((comment) => (
         <CommentItem key={comment.commentId} comment={comment} />
       ))}
@@ -21,6 +24,7 @@ const Container = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
+  align-items: center;
   overflow: auto;
   margin: 10px 0;
   padding: 20px;
@@ -35,4 +39,10 @@ const Container = styled.div`
   }
   -ms-overflow-style: none; /* IE and Edge */
   scrollbar-width: none; /* Firefox */
+`;
+
+const CommentEmpty = styled.div`
+  padding: 240px 0;
+  color: ${({ theme }) => theme.color_textGray};
+  font-size: ${({ theme }) => theme.fontSize_md};
 `;
