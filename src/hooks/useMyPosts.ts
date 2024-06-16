8cdb517dfd47188a3c4d83d88b6c9c74cabd1f6c
연하영 <dusgkdud7@naver.com>
2024-06-17 03:49:01 +0900

    [Feat/#55] 게시글 조회 react-query 적용

--- src/hooks/useMyPosts.ts
+++ src/hooks/useMyPosts.ts
@@ -1,41 +1,42 @@
 import { postAPI } from '@/api/requests/postAPI';
-import { PostSummary } from '@/models/post.model';
 import { useConfirm, usePopUpActions } from '@/store/popUpStore';
 import { useEffect, useState } from 'react';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 export const useMyPosts = () => {
   const { isConfirmed } = useConfirm();
   const { setIsConfirmOpen, setIsDeleted } = usePopUpActions();
-  const [deleteTarget, setDeleteTarget] = useState<number | null>(null);
-  const [myPosts, setMyPosts] = useState<PostSummary[] | null>(null);
+  const [deletePost, setDeletePost] = useState<number | null>(null);
+  const queryClient = useQueryClient();
 
-  const fetchMyPosts = async () => {
-    const params = { myPage: true };
-    const response = await postAPI.posts(params);
-    setMyPosts(response.result);
-  };
+  const { data: myPosts } = useQuery({
+    queryKey: ['myPosts'],
+    queryFn: async () => {
+      const response = await postAPI.posts({ myPage: true });
+      return response.result || [];
+    },
+  });
 
-  const deletePost = async () => {
-    const response = await postAPI.deletePost(deleteTarget!);
-    if (response?.isSuccess) {
+  const deletePostMutation = useMutation({
+    mutationFn: async (postId: number) => {
+      return await postAPI.deletePost(postId);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['myPosts'] });
       setIsDeleted(true);
-    }
-  };
+    },
+  });
 
   const handleDelete = (postId: number) => {
-    setDeleteTarget(postId);
+    setDeletePost(postId);
     setIsConfirmOpen(true);
   };
 
   useEffect(() => {
-    fetchMyPosts();
-  }, []);
-
-  useEffect(() => {
-    if (isConfirmed) {
-      deletePost();
+    if (isConfirmed && deletePost !== null) {
+      deletePostMutation.mutate(deletePost);
     }
-  }, [isConfirmed]);
+  }, [isConfirmed, deletePost]);
 
   return { myPosts, handleDelete };
 };
