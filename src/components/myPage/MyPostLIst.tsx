70f3ae0c9f16ffd7449a429133654a42fc918660
연하영 <dusgkdud7@naver.com>
2024-07-08 17:32:19 +0900

    [Feat/#67] Table 컴포넌트 분리

--- src/components/myPage/MyPostLIst.tsx
+++ src/components/myPage/MyPostLIst.tsx
@@ -1,6 +1,4 @@
-import styled, { css } from 'styled-components';
-import { getCategoryName } from '@/utils/getCategoryName';
-import { formatDate } from '@/utils/format';
+import styled from 'styled-components';
 import PostModal from '../modal/PostModal';
 import { useModal } from '@/hooks/useModal';
 import MyPostEmpty from './MyPostEmpty';
@@ -8,6 +6,7 @@ import { ICONS } from '@/constants/assets';
 import { useUserInfo } from '@/hooks/useUserInfo';
 import { AnimatePresence } from 'framer-motion';
 import { useMyPosts } from '@/hooks/useMyPosts';
+import Table from './Table';
 
 function MyPostList() {
   const { userData } = useUserInfo();
@@ -20,54 +19,26 @@ function MyPostList() {
     <Wrapper>
       <Title>
         <h2>내가 공유한 지식</h2>
-        <span>{userData!.totalPosts}개</span>
       </Title>
 
-      <Container>
-        {userData!.totalPosts > 0 ? (
-          <TableWrapper>
-            <Table>
-              <thead>
-                <tr>
-                  <Th className='category'>카테고리</Th>
-                  <Th className='title'>제목</Th>
-                  <Th className='createAt'>작성 일자</Th>
-                  <Th className='delete'>삭제</Th>
-                </tr>
-              </thead>
-            </Table>
-            <TBodyWrapper>
-              <Table>
-                <tbody>
-                  {myPosts?.map((post) => (
-                    <tr key={post.postId}>
-                      <Td className='category'>
-                        <Tag>{getCategoryName(post.categoryId)}</Tag>
-                      </Td>
-                      <Td
-                        className='title'
-                        onClick={() => handleClick(post.postId)}>
-                        {post.title}
-                      </Td>
-                      <Td className='createAt'>{formatDate(post.createdAt)}</Td>
-                      <Td className='delete'>
-                        <img
-                          src={ICONS.delete.black}
-                          alt='delete'
-                          style={{ cursor: 'pointer' }}
-                          onClick={() => handleDelete(post.postId)}
-                        />
-                      </Td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-            </TBodyWrapper>
-          </TableWrapper>
-        ) : (
-          <MyPostEmpty />
-        )}
-      </Container>
+      {userData!.totalPosts > 0 ? (
+        <Table
+          data={myPosts!}
+          buttonTitle='삭제'
+          button={(post) => (
+            <img
+              src={ICONS.delete.black}
+              alt='delete'
+              style={{ cursor: 'pointer' }}
+              onClick={() => handleDelete(post.postId)}
+            />
+          )}
+          handleClick={handleClick}
+        />
+      ) : (
+        <MyPostEmpty />
+      )}
+
       <AnimatePresence>
         {isModalOpen && <PostModal closeModal={closeModal} />}
       </AnimatePresence>
@@ -84,97 +55,12 @@ const Wrapper = styled.div`
 
 const Title = styled.div`
   display: flex;
-  gap: 6px;
   align-items: flex-end;
+  margin: 0 15px;
 
   & span {
     font-weight: 500;
     font-size: ${({ theme }) => theme.fontSize_base};
     color: ${({ theme }) => theme.color_textGray};
   }
-`;
-
-const Container = styled.div`
-  margin-top: 15px;
-  background-color: ${({ theme }) => theme.color_bgWhite};
-  border: 1px solid ${({ theme }) => theme.color_borderGray};
-  border-radius: 12px;
-  overflow: hidden;
-`;
-
-const TableWrapper = styled.div`
-  overflow-x: auto;
-`;
-
-const TBodyWrapper = styled.div`
-  min-height: 260px;
-  max-height: 400px;
-  overflow-y: auto;
-
-  &::-webkit-scrollbar {
-    width: 5px;
-  }
-
-  &::-webkit-scrollbar-track {
-    background: ${({ theme }) => theme.color_bgLightGray};
-  }
-
-  &::-webkit-scrollbar-thumb {
-    background: ${({ theme }) => theme.color_key};
-    border-radius: 10px;
-  }
-`;
-
-const Table = styled.table`
-  width: 100%;
-  border-collapse: collapse;
-`;
-
-const columnStyles = css`
-  padding: 15px;
-  border-bottom: 1px solid #ddd;
-  font-weight: 700;
-  text-align: center;
-  &.category {
-    width: 15%;
-  }
-  &.title {
-    width: 50%;
-    text-align: left;
-  }
-  &.createAt {
-    width: 20%;
-  }
-  &.delete {
-    width: 15%;
-  }
-`;
-
-const Th = styled.th`
-  ${columnStyles}
-`;
-
-const Td = styled.td`
-  ${columnStyles}
-
-  font-weight: 400;
-  vertical-align: middle;
-
-  &.title {
-    font-weight: 500;
-    cursor: pointer;
-  }
-
-  &.createAt {
-    color: ${({ theme }) => theme.color_textGray};
-  }
-`;
-
-const Tag = styled.span`
-  color: ${({ theme }) => theme.color_textWhite};
-  background-color: ${({ theme }) => theme.color_key};
-  font-size: ${({ theme }) => theme.fontSize_sm};
-  padding: 5px 14px;
-  border-radius: 10px;
-  font-weight: 500;
 `;
---
+++ src/components/myPage/Table.tsx
@@ -0,0 +1,118 @@
+import styled from 'styled-components';
+import { getCategoryName } from '@/utils/getCategoryName';
+import { formatDate } from '@/utils/format';
+import { PostSummary } from '@/models/post.model';
+
+interface TableProps {
+  data: PostSummary[];
+  buttonTitle: string;
+  button: (data: PostSummary) => JSX.Element;
+  handleClick: (postId: number) => void;
+}
+
+function Table({ data = [], buttonTitle, button, handleClick }: TableProps) {
+  return (
+    <Wrapper>
+      <TableContainer>
+        <table>
+          <thead>
+            <tr>
+              <th>카테고리</th>
+              <th className='title'>제목</th>
+              <th>작성일자</th>
+              <th>{buttonTitle}</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr key={item.postId}>
+                <td>
+                  <Tag>{getCategoryName(item.categoryId)}</Tag>
+                </td>
+                <td className='title' onClick={() => handleClick(item.postId)}>
+                  {item.title}
+                </td>
+                <td className='createdAt'>{formatDate(item.createdAt)}</td>
+                <td>{button(item)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </TableContainer>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  padding: 20px 0 0 0;
+`;
+
+const TableContainer = styled.div`
+  background-color: ${({ theme }) => theme.color_bgWhite};
+  border: 1px solid ${({ theme }) => theme.color_borderGray};
+  border-radius: 12px;
+  max-height: 400px;
+  overflow-y: auto;
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+
+    border-bottom: 1px solid ${({ theme }) => theme.color_borderGray};
+    background-color: ${({ theme }) => theme.color_bgWhite};
+
+    th {
+      font-weight: 700;
+
+      padding: 16px;
+      border-bottom: 1px solid ${({ theme }) => theme.color_borderGray};
+      text-align: center;
+    }
+
+    th.title {
+      text-align: left;
+    }
+
+    td {
+      font-weight: 400;
+      padding: 16px;
+      border-bottom: 1px solid ${({ theme }) => theme.color_borderGray};
+      text-align: center;
+    }
+
+    td.title {
+      font-weight: 500;
+      text-align: left;
+      cursor: pointer;
+    }
+
+    td.createdAt {
+      color: ${({ theme }) => theme.color_textGray};
+    }
+
+    thead {
+      position: sticky;
+      top: 0;
+      background-color: ${({ theme }) => theme.color_bgWhite};
+      z-index: 1;
+    }
+  }
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+`;
+
+const Tag = styled.span`
+  color: ${({ theme }) => theme.color_textWhite};
+  background-color: ${({ theme }) => theme.color_key};
+  font-size: ${({ theme }) => theme.fontSize_sm};
+  padding: 5px 14px;
+  border-radius: 10px;
+  font-weight: 500;
+`;
+
+export default Table;
